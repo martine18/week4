771ca1bd0f4c0e88775fa227987d9966d98d7b78
Martine <89096155+martine18@users.noreply.github.com>
2023-02-10 05:37:56 -0800

    Add files via upload

---
+++ correctionday1xp/day1xpcorrection.py
@@ -0,0 +1,46 @@
+
+#section 1
+print("Hello world" * 4)
+
+
+
+
+
+#section 5
+
+name = "martine"
+age = 20
+shoe_size = 1.20
+info = f"Hi, I'm {name}, {age} I am a software developer{shoe_size}"
+print(info)
+
+
+#section 6
+
+a = 10
+b = 12
+if a > b:
+    print("Hello World")
+#section 9
+
+user_height_in = float(input("Enter your height: "))
+user_height_cm = user_height_in * 2.54
+
+if user_height_cm > 145:
+    print("You are tall enough to ride")
+else:
+    print("you are not allow to ride")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
